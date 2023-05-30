5660091a06a9464d20de00f482f53eca84d55e78
Oles <oles1stepanov@gmail.com>
2023-05-30 21:28:06 +0300

    Create connection from page to database

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "app123"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
 db = SQLAlchemy(app)
+
 
 class Form(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -22,8 +24,19 @@ def index():
         last_name = request.form["last_name"]
         email = request.form["email"]
         date = request.form["date"]
+        date_obj = datetime.strptime(date, "%Y-%m-%d")
         occupation = request.form["occupation"]
-        print(first_name, last_name, email, date, occupation)
+
+        form = Form(first_name=first_name,
+                    last_name=last_name,
+                    email=email,
+                    date=date_obj,
+                    occupation=occupation)
+
+        db.session.add(form)
+        db.session.commit()
+
+
 
     return render_template("index.html")
 
